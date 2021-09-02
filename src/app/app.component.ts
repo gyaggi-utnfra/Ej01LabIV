250cc13c29194ff8c43dc787684b84d188c9348c
gyaggi-utnfra <gustavo_yaggi@hotmail.com>
2021-09-02 18:25:33 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ej01LabIV';
+  public title = 'Ej01LabIV';
+  public edadUno = 0;
+  public edadDos = 0;
+  public promedio: number = 0;
+  public suma: number = 0;
+  public mostrar: boolean = false;
+
+  public calcular(): void{
+    this.promedio = (this.edadUno + this.edadDos)/2;
+    this.suma = this.edadUno + this.edadDos;
+    this.mostrar = true;
+  }
+
+  public limpiar(): void{
+    this.edadUno=0;
+    this.edadDos=0;
+    this.promedio=0;
+    this.suma=0;
+    this.mostrar=false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './toDo/login/login.component';
+import { BienvenidoComponent } from './toDo/bienvenido/bienvenido.component';
+import { ErrorComponent } from './toDo/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BienvenidoComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
